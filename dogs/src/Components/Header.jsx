8f88a3d68073b56997642e9eed6b0de098da0b55
Felipe Godoy <felipedegodoy.dev@gmail.com>
2026-08-12 16:44:36 -0300

    start initial structure

---
+++ dogs/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>Header
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login / Criar</Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
