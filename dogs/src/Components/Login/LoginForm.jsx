16825d1ce99faddebcdd455c5d0d49216151f459
Felipe Godoy <felipedegodoy.dev@gmail.com>
2026-08-13 20:52:44 -0300

    create Input and Button

--- dogs/src/Components/Login/LoginForm.jsx
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 
 const LoginForm = () => {
   const [username, setUsername] = React.userState("");
@@ -28,21 +30,8 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => {
-            setUsername(target.value);
-          }}
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => {
-            setPassword(target.value);
-          }}
-        />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="name" />
+        <Button>Entrar</Button>
       </form>
       <Link to="login/criar">Cadstro</Link>
     </section>
