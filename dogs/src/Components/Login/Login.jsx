12169669ca8bf0015eae9abc20599a24c086a7f3
Felipe Godoy <felipedegodoy.dev@gmail.com>
2026-08-17 18:24:25 -0300

    add context

--- dogs/src/Components/Login/Login.jsx
+++ dogs/src/Components/Login/Login.jsx
@@ -1,10 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
